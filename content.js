0b701e03277d958d9c45f07c01606cff11480b02
Dag Karlsson <dag-karlsson@hotmail.com>
2022-09-21 00:03:27 +0300

    Add nice image

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 function removePaywallAlandstidningen() {
   const paywall = document.getElementById('tulo-paywall-block-v2');
-  paywall.style.display = 'none'
-
+  //paywall.style.display = 'none'
+  paywall.innerHTML = '<h2>Keep on reading</h2><img id="myImage" src="https://thumbs.dreamstime.com/b/smiley-face-thumbs-up-14491322.jpg">'
   const collection = document.getElementsByClassName("behind-paywall");
   for (let i = 0; i < collection.length; i++) {
     collection[i].style.display = "block";
--- popup.js
+++ popup.js
@@ -3,14 +3,14 @@ changeColor.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    func: removePaywall,
+    func: removePaywallAlandstidningen,
   });
 });
 
-function removePaywall() {
+function removePaywallAlandstidningen() {
   const paywall = document.getElementById('tulo-paywall-block-v2');
-  paywall.style.display = 'none'
-
+  //paywall.style.display = 'none'
+  paywall.innerHTML = '<h2>Keep on reading</h2><img id="myImage" src="https://thumbs.dreamstime.com/b/smiley-face-thumbs-up-14491322.jpg">'
   const collection = document.getElementsByClassName("behind-paywall");
   for (let i = 0; i < collection.length; i++) {
     collection[i].style.display = "block";
